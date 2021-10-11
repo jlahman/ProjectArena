758c3327914d29a864021dec17cc577c8bb88c8c
Keilith Lakeson <keilith.lakeson@gmail.com>
2021-10-11 02:07:05 -0400

    Decreased deadzone on aim stick input actions.

--- Player/Weapons/SingleFire/SingleFire.gd
+++ Player/Weapons/SingleFire/SingleFire.gd
@@ -15,5 +15,5 @@ func get_input():
 	var horizontal = Input.get_action_strength("look_right") - Input.get_action_strength("look_left")
 	var vertical = Input.get_action_strength("look_down") - Input.get_action_strength("look_up")
 	if(horizontal != 0.0 or vertical != 0.0):
-		_look_dir = Vector2(horizontal, vertical).normalized()
+		_look_dir = (_look_dir + Vector2(horizontal, vertical).normalized())/2
 
